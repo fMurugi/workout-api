b32546f08fa9e698bae74df5621dfc59b11c5879
fmurugi <fionamurugi99@gmail.com>
2025-04-19 15:30:25 +0300

    add squlite and GORM

--- data/memory.go
+++ data/memory.go
@@ -1,5 +1,24 @@
 package data
 
-import "workout-api/models"
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"log"
+	"workout-api/models"
+)
+
+var DB *gorm.DB
+func InitDB(){
+	var err error
+	DB, err = gorm.Open(sqlite.Open("workouts.db"),&gorm.Config{})
+	if err != nil{
+		log.Fatal("failed to connect database:", err)
+	}
+
+	err = DB.AutoMigrate(&models.Workout{})
+	if err != nil{
+		log.Fatal("failed to migrate database:", err)
+	}
+}
 
 var Workouts = make(map[string]models.Workout)
